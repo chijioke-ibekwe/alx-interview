e9c9787022d47e0c9e8f3cd8e0a747e70c6632d8
Chijioke Ibekwe <chijioke.ibekwe@sabiroad.com>
2022-11-16 04:02:24 +0100

    Implement log parsing solution

--- 0x03-log_parsing/0-generator.py
+++ 0x03-log_parsing/0-generator.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""
+Main file for log parsing
+solution
+"""
 import random
 import sys
 from time import sleep
@@ -12,3 +16,4 @@ for i in range(10000):
         random.choice([200, 301, 400, 401, 403, 404, 405, 500]),
         random.randint(1, 1024)
     ))
+    
