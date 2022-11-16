68aab641f47e44f07c1fbb515c03acda11f791ab
Chijioke Ibekwe <chijioke.ibekwe@sabiroad.com>
2022-11-16 04:00:31 +0100

    Implement log parsing solution

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,35 +1,17 @@
 #!/usr/bin/python3
 """
-File containing minOperations function
+Minimum operations
 """
 
 
-def is_prime(n):
-    return all(n%i for i in range(2, int(n**0.5)+1))
-
-def hcf(n):
-    if is_prime(n):
-        return n
-
-    if n%2 == 0:
-        return n/2
-
-    num = 0
-    for i in range(3, int(n/2) + 2):
-        if n%i == 0:
-            num = i
-    
-    return num
-
 def minOperations(n):
-    if n <= 1 or n%1 != 0:
+    """
+
+    :param n:
+    :return:
+    """
+    if n <= 1:
         return 0
-
-    if is_prime(n):
-        return n
-
-    num = hcf(n)
-    while(num == n/2 and (is_prime(num) is False)):
-        num = hcf(num)
-    
-    return int(num + n/num)
+    for op in range(2, n+1):
+        if n % op == 0:
+            return minOperations(int(n/op)) + op
