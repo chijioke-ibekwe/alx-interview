dc13768e60cc9d7f50f47b169c9fc1be258f6da6
Chijioke Ibekwe <chijioke.ibekwe@sabiroad.com>
2022-12-01 22:05:31 +0100

    Update n-queens solution

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -19,53 +19,35 @@ if float(argument) < 4:
     print("N must be at least 4")
     sys.exit(1)
 
-def solveNQueens(n):
-    """Solution for n queens"""
-    col = set()  # keep track of used columns
-    pos = set()  # (r + c) keep track of used positive diagonals
-    neg = set()  # (r - c) keep track of used negative diagonals
-
-    res = []  # final result
-
-    board = [[] for n in range(n)]  # create empy board
-
-    def backtrack(row):
-        """function for recursion"""
-        # means we've reached the last row
-        if row == n:
-            # get copy of current solution(current board)
-            copy = board.copy()
-            res.append(copy)
-            return
-
-        # for every column
-        for c in range(n):
-            # if we find that the column or diagonals are used, then skip
-            if c in col or (row + c) in pos or (row - c) in neg:
-                continue
-
-            # register found columns and diagonals
-            col.add(c)
-            pos.add(row + c)
-            neg.add(row - c)
-
-            board[row] = [row, c]
-
-            # move to next row
-            backtrack(row + 1)
-
-            # finally undo
-            col.remove(c)
-            pos.remove(row + c)
-            neg.remove(row - c)
-            board[row] = []
-
-    backtrack(0)
-
-    return res
+def solve_nqueens(n):
+    '''self descriptive'''
+    if n == 0:
+        return [[]]
+    inner_solution = solve_nqueens(n - 1)
+    return [solution + [(n, i + 1)]
+            for i in range(argument)
+            for solution in inner_solution
+            if safe_queen((n, i + 1), solution)]
 
 
-if __name__ == "__main__":
-    boards = solveNQueens(argument)
-    for board in boards:
-        print(board)
+def attack_queen(square, queen):
+    '''self descriptive'''
+    (row1, col1) = square
+    (row2, col2) = queen
+    return (row1 == row2) or (col1 == col2) or\
+        abs(row1 - row2) == abs(col1 - col2)
+
+
+def safe_queen(sqr, queens):
+    '''self descriptive'''
+    for queen in queens:
+        if attack_queen(sqr, queen):
+            return False
+    return True
+
+
+for answer in reversed(solve_nqueens(argument)):
+    result = []
+    for p in [list(p) for p in answer]:
+        result.append([i - 1 for i in p])
+    print(result)  
